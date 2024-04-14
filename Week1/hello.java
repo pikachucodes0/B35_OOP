483a0263008809a4303a97aeb64609ae1c9608c4
Aryan <aaryan.nakarmi111@gmail.com>
2024-04-14 14:22:39 +0545

    Week1 Task1

--- Week1/hello.java
+++ Week1/hello.java
@@ -1,5 +1,6 @@
 package Week1;
 public class hello{
+    /*Write a program that prints Hello World! to the console. Save the program as Hello.java.*/
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
--- Week1/pattern.java
+++ Week1/pattern.java
@@ -1,10 +1,20 @@
 package Week1;
 public class pattern {
     public static void main(String[] args) {
+    /*4. WAP that prints the following pattern:
+
+    *
+
+    **
+
+    ***
+
+    ****/
         for(int i=1;i<5;i++){
-            for(int j=1;j<i;j++){
-                System.out.println("*");
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
             }
+            System.out.println("\n");
         }
     }
 }
